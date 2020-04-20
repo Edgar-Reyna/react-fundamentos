28b1412d0a912b7a06fbffe10883da8daad5f5c4
Edgar Reyna <edgar.reyna.a@gmail.com>
2020-04-19 23:42:14 -0500

    fundamentos react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const TarjetaFruta=(props)=>(
+  <div> 
+    <h3>{props.name}</h3>
+    <hr/>
+    <p>$ {props.price}</p>
+  </div>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var frutaExotica='Fruta del Dragon'
+
+const App=()=>(
+  <div>
+    <TarjetaFruta name={'Sandia'} price={2.00}/>
+    <TarjetaFruta name={'Naranja'} price={1.50}/>
+    <TarjetaFruta name={`${frutaExotica}`} price={3.30}/>
+  </div>
+)
+
+ReactDOM.render(<App/>,document.getElementById('root'))
